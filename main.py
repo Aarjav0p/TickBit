3a3b48a706153f463b81272eff8a27faf03ab2c8
Aarjav0p <aarjavpatriya74@gmail.com>
2026-04-26 13:17:27 +0530

    Added help command and mention for bot

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import discord
 from discord import app_commands
 import os
+from dotenv import load_dotenv
 
-TOKEN = os.getenv("DISCORD_CLIENT")
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
 GUILD_ID = discord.Object(id=1236902125406257192)
+
+HELP_TEXT=(
+    "** TickBit help menu **\n"
+    "/ping → Check if bot is alive\n"
+    "/echo <message> → Echo your message\n"
+    "/help → Show this help message\n\n"
+    "You can also mention me with `help`!"
+)
 
 class Client(discord.Client):
     async def on_ready(self):
@@ -21,6 +32,10 @@ class Client(discord.Client):
         
         if message.content.startswith("Hello"):
             await message.channel.send(f"Hi there {message.author}")
+        
+        if self.user in message.mentions:
+            if "help" in message.content.lower():
+                await message.channel.send(HELP_TEXT)
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -32,8 +47,12 @@ tree = app_commands.CommandTree(client)
 async def ping(interaction: discord.Interaction):
     await interaction.response.send_message("Pong!")
     
-@tree.command(name="call", description="Prints the given message", guild=GUILD_ID)
-async def call(interaction: discord.Interaction, message: str):
+@tree.command(name="echo", description="Prints the given message", guild=GUILD_ID)
+async def echo(interaction: discord.Interaction, message: str):
     await interaction.response.send_message(message)
+
+@tree.command(name="help", description="Tells status and all commands", guild=GUILD_ID)
+async def help(interaction: discord.Interaction):
+    await interaction.response.send_message(HELP_TEXT)
 
 client.run(TOKEN)
